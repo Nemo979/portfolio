e492cde4a22ace1b957bde4413ba8494d6bd59c9
Nemo <xugan_nemo@163.com>
2019-05-22 21:47:19 +0800

    添加了上传图片 和 修改标题 的功能

--- gallery/models.py
+++ gallery/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+class Gallery(models.Model): # model改了一定要重新迁移到数据库中
+    description = models.CharField(default='这里写作品介绍',max_length=100)
+    image = models.ImageField(default='default.png',upload_to='images/')
+    tital = models.CharField(default='作品标题',max_length=50)
+
+    def __str__(self):
+        return self.tital
